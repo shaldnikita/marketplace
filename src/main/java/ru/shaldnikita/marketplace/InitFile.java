b63fd8d0429811ec8eb266bbdb9913ccf29e46f2
Artyom <artem.pashinin@gmail.com>
2019-11-23 16:06:50 +0300

    revert init file

--- src/main/java/ru/shaldnikita/marketplace/InitFile.java
+++ src/main/java/ru/shaldnikita/marketplace/InitFile.java
@@ -1,52 +1,52 @@
-//package ru.shaldnikita.marketplace;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//import ru.shaldnikita.marketplace.domain.Item;
-//import ru.shaldnikita.marketplace.domain.ItemRepository;
-//
-//import java.util.UUID;
-//
-//@Component
-//public class InitFile implements CommandLineRunner {
-//
-//
-//    @Autowired
-//    private ItemRepository repository;
-//
-//    @Override
-//    public void run(String... args) {
-//        Item item1 = new Item(
-//                UUID.randomUUID().toString(),
-//                "Item1",
-//                "Item1 description",
-//                100000,
-//                0,
-//                new byte[]{1, 2, 3}
-//        );
-//
-//
-//        Item item2 = new Item(
-//                UUID.randomUUID().toString(),
-//                "Item2",
-//                "Item2 description",
-//                200000,
-//                5,
-//                new byte[]{1, 2, 3}
-//        );
-//
-//        Item item3 = new Item(
-//                UUID.randomUUID().toString(),
-//                "Item3",
-//                "Item3 description",
-//                300000,
-//                3,
-//                new byte[]{1, 2, 3}
-//        );
-//
-//        repository.save(item1);
-//        repository.save(item2);
-//        repository.save(item3);
-//    }
-//}
+package ru.shaldnikita.marketplace;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import ru.shaldnikita.marketplace.domain.Item;
+import ru.shaldnikita.marketplace.domain.ItemRepository;
+
+import java.util.UUID;
+
+@Component
+public class InitFile implements CommandLineRunner {
+
+
+    @Autowired
+    private ItemRepository repository;
+
+    @Override
+    public void run(String... args) {
+        Item item1 = new Item(
+                UUID.randomUUID().toString(),
+                "Item1",
+                "Item1 description",
+                100000,
+                0,
+                new byte[]{1, 2, 3}
+        );
+
+
+        Item item2 = new Item(
+                UUID.randomUUID().toString(),
+                "Item2",
+                "Item2 description",
+                200000,
+                5,
+                new byte[]{1, 2, 3}
+        );
+
+        Item item3 = new Item(
+                UUID.randomUUID().toString(),
+                "Item3",
+                "Item3 description",
+                300000,
+                3,
+                new byte[]{1, 2, 3}
+        );
+
+        repository.save(item1);
+        repository.save(item2);
+        repository.save(item3);
+    }
+}
